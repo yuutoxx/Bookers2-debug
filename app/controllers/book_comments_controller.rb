64f625b07422640d54016ce765ce120c2389887d
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-02-01 13:23:06 +0000

    [add]comment

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,15 @@
+class BookCommentsController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to book_path(book)
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
+  has_many :book_comments,dependent: :destroy
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
